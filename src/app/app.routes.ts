2d8621567d6d982e253f9d3db715927a842a1e01
DupLeo <109819277+DupLeo@users.noreply.github.com>
2025-04-01 16:26:00 +0200

    add style login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
 import {AppComponent} from './app.component';
 import {UsersComponent} from './pages/login/users/users.component';
+import {AccueilComponent} from './pages/accueil/accueil.component';
 
-export const routes: Routes = [{
-  path: '',
-  redirectTo: 'home',
-  pathMatch: 'full',
-},{
-  path: 'users',
-  component: UsersComponent,
-}];
+export const routes: Routes = [
+  { path: '', component: AccueilComponent }, // Page d'accueil par défaut
+  { path: 'login', component: UsersComponent }, // Page de connexion
+];
